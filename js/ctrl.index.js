0ea1e4223a68af5daaa0cf13e435c47bf28addc7
Nate Beatty <nate.beatty@gmail.com>
2014-02-18 11:52:46 -0500

    lazy image loading

--- js/ctrl.index.js
+++ js/ctrl.index.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
+$(window).load(function() {
 
-	// $('.splash-container .background').fadeOut(0);
+	$('.splash-container .background').fadeOut(0);
 
-	// var url = 'img/landing_bg.jpg';
-	// $.ajax({ 
-	// 	url : url, 
-	// 	cache: true,
-	// 	processData : false,
-	// }).always(function(){
-	// 	$('.splash-container .background-unloaded').css('background-image', 'url(' + url + ')');
+	var url = 'img/landing_bg.jpg';
+	$.ajax({ 
+		url : url, 
+		cache: true,
+		processData : false,
+	}).always(function(){
+		$('.splash-container .background-unloaded').css('background-image', 'url(' + url + ')');
 
-	// 	$('.splash-container .background-unloaded').css({
-	// 		'opacity' : 0.3,
-	// 		'-ms-filter' : 'progid:DXImageTransform.Microsoft.Alpha(Opacity=30)',
-	// 		'filter' : 'alpha(opacity=50)',
-	// 		'-moz-opacity' : 0.5,
-	// 		'-khtml-opacity' : 0.5
-	// 	});
-	// });
+		$('.splash-container .background-unloaded').css({
+			'opacity' : 0.3,
+			'-ms-filter' : 'progid:DXImageTransform.Microsoft.Alpha(Opacity=30)',
+			'filter' : 'alpha(opacity=50)',
+			'-moz-opacity' : 0.5,
+			'-khtml-opacity' : 0.5
+		});
+	});
 });
