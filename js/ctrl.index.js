477162f10960c9771a5a6394f3d76df614576ca1
Nate Beatty <nate.beatty@gmail.com>
2014-02-18 11:47:57 -0500

    Bg image removed

--- js/ctrl.index.js
+++ js/ctrl.index.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
 
 	// $('.splash-container .background').fadeOut(0);
 
-	var url = 'img/landing_bg.jpg';
-	$.ajax({ 
-		url : url, 
-		cache: true,
-		processData : false,
-	}).always(function(){
-		$('.splash-container .background-unloaded').css('background-image', 'url(' + url + ')');
+	// var url = 'img/landing_bg.jpg';
+	// $.ajax({ 
+	// 	url : url, 
+	// 	cache: true,
+	// 	processData : false,
+	// }).always(function(){
+	// 	$('.splash-container .background-unloaded').css('background-image', 'url(' + url + ')');
 
-		$('.splash-container .background-unloaded').css({
-			'opacity' : 0.3,
-			'-ms-filter' : 'progid:DXImageTransform.Microsoft.Alpha(Opacity=30)',
-			'filter' : 'alpha(opacity=50)',
-			'-moz-opacity' : 0.5,
-			'-khtml-opacity' : 0.5
-		});
-	});
+	// 	$('.splash-container .background-unloaded').css({
+	// 		'opacity' : 0.3,
+	// 		'-ms-filter' : 'progid:DXImageTransform.Microsoft.Alpha(Opacity=30)',
+	// 		'filter' : 'alpha(opacity=50)',
+	// 		'-moz-opacity' : 0.5,
+	// 		'-khtml-opacity' : 0.5
+	// 	});
+	// });
 });
